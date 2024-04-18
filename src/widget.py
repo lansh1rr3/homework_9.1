050849e1f455500e335f2fa2012cf44361d985ed
Maxim Razboykin <maksfanloker@gmail.com>
2024-04-18 13:42:39 +0300

    Initial commit

--- src/widget.py
+++ src/widget.py
@@ -3,6 +3,8 @@ from src.masks import mask_for_bill_number, mask_for_number_card
 
 def mask_of_card_number_and_bill_number(input_number: str) -> str:
     num = input_number.split()
+    """Принимает на вход строку с информацией — тип карты/счета и номер карты/счета.
+    Возвращает исходную строку с замаскированным номером карты/счета"""
     if "Счет" in input_number:
         mask = f"Счёт {mask_for_bill_number(num[-1])}"
     else:
@@ -11,5 +13,6 @@ def mask_of_card_number_and_bill_number(input_number: str) -> str:
 
 
 def data_split(data: str) -> str:
+    """возвращает дату в нужном формате"""
     split_data_day_month_year = data.split("T")[0].split("-")
     return f"{split_data_day_month_year[-1]}.{split_data_day_month_year[-2]}.{split_data_day_month_year[-3]}"
