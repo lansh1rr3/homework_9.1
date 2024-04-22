bb7164106062dcdb25484b403597f68471f3f0c5
Maxim Razboykin <maksfanloker@gmail.com>
2024-04-22 18:00:23 +0300

    add tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,4 +1,11 @@
-from src.widget import data_split, mask_of_card_number_and_bill_number
+from src.processing import filter_by_state, sort_by_date
 
-print(mask_of_card_number_and_bill_number(input("Введите номер карты или счёта:")))
-print(data_split(input("Введите дату:")))
+data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+print(sort_by_date(data))
+print(filter_by_state(data))
