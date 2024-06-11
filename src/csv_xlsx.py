b5c1f3903f15c5eab840512d5788aadd53816a5a
Maxim Razboykin <maksfanloker@gmail.com>
2024-06-11 16:45:10 +0300

    update_13.1

---
+++ src/csv_xlsx.py
@@ -0,0 +1,24 @@
+import csv
+from typing import Any
+import pandas as pd
+
+
+def read_csv(file_path: str) -> Any:
+    """
+    Считывает финансовые операции из CSV-файла.
+    """
+    with open(file_path, encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for row in reader:
+            print(row)
+
+
+def read_xlsx(file_path: str) -> Any:
+    """
+    Считывает финансовые операции из XLSX-файла.
+    """
+    df = pd.read_excel(file_path)
+    return df.to_dict("records")
+
+
+print(read_xlsx("../data/transactions_excel.xlsx"))
---
+++ tests/test_csv_xlsx.py
@@ -0,0 +1,36 @@
+import unittest
+from unittest.mock import patch, mock_open
+from src.csv_xlsx import read_csv, read_xlsx
+import pandas as pd
+
+
+class TestReadCSV(unittest.TestCase):
+
+    @patch('builtins.open', new_callable=mock_open, read_data='Date;Description;Amount\n2022-01-01;Salary;1000\n')
+    @patch('csv.DictReader')
+    def test_read_csv(self, mock_csvreader, mock_open):
+        """
+        Тестовый пример для функции read_csv.
+        """
+        file_path = "test.csv"
+        read_csv(file_path)
+        mock_open.assert_called_once_with(file_path, encoding='utf-8')
+        mock_csvreader.assert_called_once()
+
+
+class TestReadXLSX(unittest.TestCase):
+
+    @patch('pandas.read_excel',
+           return_value=pd.DataFrame({'Date': ['2022-01-01'], 'Description': ['Salary'], 'Amount': [1000]}))
+    def test_read_xlsx(self, mock_read_excel):
+        """
+        Тестовый пример для функции read_xlsx.
+        """
+        file_path = "test.xlsx"
+        result = read_xlsx(file_path)
+        mock_read_excel.assert_called_once_with(file_path)
+        self.assertEqual(result, [{'Date': '2022-01-01', 'Description': 'Salary', 'Amount': 1000}])
+
+
+if __name__ == '__main__':
+    unittest.main()
